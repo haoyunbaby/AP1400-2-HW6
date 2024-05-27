e52621884a0d0d6f0a01b296dd061b8c43150c49
Zhikai Xing <haoyunbaobei1209@gmail.com>
2024-05-27 22:44:22 +0800

    Update ans
    pass all unit test

--- include/q1.h
+++ include/q1.h
@@ -1,4 +1,29 @@
 #ifndef Q1_H
 #define Q1_H
+#include <cmath>
+#include <functional>
+namespace q1 {
+double gradient_descent(double init, double step,
+                        std::function<double(double)> func);
+const double delta = 1e-3;
+const double epsilon = 1e-10;
+double derivative(double x, const std::function<double(double)> &func) {
+  return (func(x + delta) - func(x - delta)) / (2 * delta);
+}
+template <typename T, typename F>
+T gradient_descent(T init, T step) {
+  return gradient_descent(init, step, F());
+}
+double gradient_descent(double init, double step,
+                        std::function<double(double)> func) {
+  double cur = init;
+  double d = derivative(cur, func);
+  while (std::abs(d) > epsilon) {
+    cur = cur - step * d;
+    d = derivative(cur, func);
+  }
+  return cur;
+}
 
-#endif //Q1_H
+}  // namespace q1
+#endif  // Q1_H
